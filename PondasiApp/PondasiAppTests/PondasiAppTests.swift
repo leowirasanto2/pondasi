ce2f1e837f9e7b7d684f4b6c6d3fdac354af6a51
Leo Wirasanto Laia <leowirasanto@gmail.com>
2026-04-02 22:27:53 +0800

    create PondasiApp

---
+++ PondasiApp/PondasiAppTests/PondasiAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  PondasiAppTests.swift
+//  PondasiAppTests
+//
+//  Created by Leo Wirasanto Laia on 02/04/26.
+//
+
+import Testing
+@testable import PondasiApp
+
+struct PondasiAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
